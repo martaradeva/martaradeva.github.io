79bb6d236926c13209769ae80a30d462f044628f
Martina Radeva <martaradeva@yahoo.com>
2014-04-16 21:10:03 +0300

    using hash keys to create urls!

--- generate.rb
+++ generate.rb
@@ -1,23 +1,23 @@
 # encoding: utf-8
 wsbody = File.read('template.html')
 languages = {
-	:bgtext => {
+	'bg' => {
 		:name_title => 'Мартина Радева',
 		:text => 'Добре дошли на личната ми страница.',
 		:text1 => 'Тук може да намерите връзки за присъствието ми в интернет.',
 		:hlink => '<a  href="../en/index.html">This site in English</a>',
-		:file_url => ".\\bg\\index.html"
 	},
-	:entext => {
+	'en' => {
 		:name_title => 'Martina Radeva',
 		:hlink => '<a  href="../bg/index.html">Страницата на български</a>',
 		:text => 'Welcome to my personal homepage.',
 		:text1 => 'Here you can find links to my profile in various social networks.',
-		:file_url => ".\\en\\index.html"
 	},
 }
 languages.each do |language, content|
 	temp_text = wsbody.%(content)
-	# puts temp_text
-	File.write(content[:file_url], temp_text)
+	url = '.\\' + language + '\\index.html'
+	puts url
+	puts temp_text
+	File.write(url, temp_text)
 end
