fdb603054f93be797264773fe66d19c3340ae1b3
Martina Radeva <martaradeva@yahoo.com>
2014-04-23 20:32:18 +0300

    YAML.LOAD

--- ruby/generate.rb
+++ ruby/generate.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 require 'yaml'
-wsbody = File.read('template.html')
+wsbody = File.read('..\template.html')
 # languages = {
 # 	:bg => {
 # 		:name_title => 'Мартина Радева',
@@ -17,16 +17,13 @@ wsbody = File.read('template.html')
 # 		:language_link_text => 'Страницата на български',
 # 	},
 # }
-temp_yaml = YAML.load_file(".\\libraries\\translations.yml")
-# puts temp_yaml.inspect
-rrr = temp_yaml.inspect
-languages = eval(rrr)
-# puts languages
+# temp_yaml = YAML.load_file("..\\libraries\\translations.yml")
+languages = YAML.load_file("..\\libraries\\translations.yml")
+# rrr = temp_yaml.inspect
+# languages = eval(rrr)
 languages.each do |language, content|
 	content[:current_language] = language.to_s 
 	temp_text = wsbody % content
-	path = '.\\' + language.to_s + '\\index.html'
-	# puts path
-	# puts temp_text
+	path = '..\\' + language.to_s + '\\index.html'
 	File.write(path, temp_text)
 end
