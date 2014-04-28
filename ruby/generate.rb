ae91da1ccb43cf30eab67d99180487247c6eeb78
Martina Radeva <martaradeva@yahoo.com>
2014-04-28 22:13:06 +0300

    generate.rb working with 2 languages AND ERB template

--- ruby/generate.rb
+++ ruby/generate.rb
@@ -1,11 +1,12 @@
 # encoding: utf-8
 require 'yaml'
+require 'erb'
 
-wsbody = File.read('..\template.html')
+wsbody = File.read('..\template.html.erb')
 languages = YAML.load_file("..\\libraries\\translations.yml")
-languages.each do |language, content|
-	content[:current_language] = language.to_s 
-	temp_text = wsbody % content
-	path = '..\\rubytest\\' + language.to_s + '\\index.html'
+languages.each do |language_current, content|
+	 content[:id] = language_current.to_s 
+	 temp_text = ERB.new(wsbody).result(binding)
+	path = '..\\' + language_current.to_s + '\\index.html'
 	File.write(path, temp_text)
 end
--- ruby/parse_erb.rb
+++ ruby/parse_erb.rb
@@ -5,7 +5,9 @@ languages = YAML.load_file("..\\libraries\\translations.yml")
 language_current = languages[:bg]
 # puts language_current
 erb_template = File.read('..\template.html.erb')
-test_blah = ERB.new(erb_template)
-test_string = test_blah.result
+test_string = ERB.new(erb_template).result
+puts test_string
+# test_blah = ERB.new(erb_template)
+# test_string = test_blah.result
 # puts test_string
-File.write('..\rubytest\index.html', test_string)
+# File.write('..\rubytest\index.html', test_string)
