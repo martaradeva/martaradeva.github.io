d2b41506ed0a71c57d554eecfb6a8829d55ff408
Martina Radeva <martaradeva@yahoo.com>
2014-04-28 22:26:18 +0300

    tweaked variable names

--- ruby/generate.rb
+++ ruby/generate.rb
@@ -4,9 +4,9 @@ require 'erb'
 
 wsbody = File.read('..\template.html.erb')
 languages = YAML.load_file("..\\libraries\\translations.yml")
-languages.each do |language_current, content|
-	 content[:id] = language_current.to_s 
-	 temp_text = ERB.new(wsbody).result(binding)
-	path = '..\\' + language_current.to_s + '\\index.html'
+languages.each do |language, content|
+	content[:id] = language.to_s 
+	temp_text = ERB.new(wsbody).result(binding)
+	path = '..\\' + content[:id] + '\\index.html'
 	File.write(path, temp_text)
 end
