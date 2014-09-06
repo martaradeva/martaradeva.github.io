6e322adb4ac309a157277610ef3155137902d744
Martina Radeva <martaradeva@yahoo.com>
2014-09-06 22:40:35 +0300

    favicon

--- ruby/parse_erb.rb
+++ ruby/parse_erb.rb
@@ -6,7 +6,7 @@ language_current = languages[:bg]
 # puts language_current
 erb_template = File.read('..\template.html.erb')
 test_string = ERB.new(erb_template).result
-puts test_string
+# puts test_string
 # test_blah = ERB.new(erb_template)
 # test_string = test_blah.result
 # puts test_string
